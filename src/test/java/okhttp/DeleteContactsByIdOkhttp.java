02ea1f868f6807a1786814f71243df5080c5808b
Katerina-09 <firiall68@gmail.com>
2025-03-31 12:05:50 +0300

    With BeforMethod

--- src/test/java/okhttp/DeleteContactsByIdOkhttp.java
+++ src/test/java/okhttp/DeleteContactsByIdOkhttp.java
@@ -3,10 +3,7 @@ package okhttp;
 import com.google.gson.Gson;
 import dto.ErrorDTO;
 import dto.MessageDto;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import okhttp3.*;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,13 +14,48 @@ public class DeleteContactsByIdOkhttp {
     String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwic3ViIjoiZmlyaWFsbDY4QGdtYWlsLmNvbSIsImlzcyI6IlJlZ3VsYWl0IiwiZXhwIjoxNzQzOTU0ODcwLCJpYXQiOjE3NDMzNTQ4NzB9.Iu3y94IfZYmL5gXaTmHRVguxnpuhbucu_88YeYYIuQA";
     Gson gson = new Gson();
     OkHttpClient client = new OkHttpClient();
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     String id;
 
     @BeforeMethod
-    public void preCondition(){
-        //create contact //
-        //get id from message:"message": "Contact was added! ID: a4a33d33-b00e-4049-8570-dfd07aace9c7"
-        //id = ""
+    public void preCondition() throws IOException{
+        String name = "Name" + System.currentTimeMillis();
+        String lastName = "LastName" + System.currentTimeMillis();
+        String email = "Email" + System.currentTimeMillis() + "@gmail.com";
+        String phone = "1234567890" + System.currentTimeMillis();
+        String address = "Address" + System.currentTimeMillis();
+
+
+        String json = "{"
+                + "\"name\": \"" + name + "\","
+                + "\"lastName\": \"" + lastName + "\","
+                + "\"email\": \"" + email + "\","
+                + "\"phone\": \"" + phone + "\""
+                + "\"address\": \"" + address + "\""
+                + "}";
+
+        RequestBody body = RequestBody.create(json, JSON);
+
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
+                .post(body)
+                .addHeader("Authorization", token)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        Assert.assertTrue(response.isSuccessful(), "contact not created");
+        String responseBody = response.body().string();
+        MessageDto messageDto = gson.fromJson(responseBody, MessageDto.class);
+
+        String message = messageDto.getMessage();
+        String[] parts = message.split("ID: ");
+        if (parts.length > 1) {
+            id = parts[1].trim();
+        } else {
+            Assert.fail("failed to extract ID: " + message);
+        }
+        System.out.println("contact created with ID : " + id);
+
     }
 
     @Test
